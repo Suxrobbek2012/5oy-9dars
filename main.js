d348cbf1db1f9591148bde0deb9b6035178cfcde
Suxrobbek2012 <suxrobbek714@gmail.com>
2025-02-25 10:52:22 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,213 @@
+// const oquvchilar = [
+//     {
+//        login:2345,
+//        parol:99998,
+//        ismi:"Nodir",
+//        familya:"Baxtiyorov",
+//        coin:1275,
+//        ustoz:"Sunnatbek Krasafchik",
+//        guruh:2094,
+//        yonalish:"Frontend",
+//        quvvat:5998,
+//        qarzdorlik:"true"
+//     },
+
+//         {
+//            login:3428,
+//            parol:88827,
+//            ismi:"suxrobbek",
+//            familya:"Baxtiyorov",
+//            coin:2500,
+//            ustoz:"Sunnatbek",
+//            guruh:2094,
+//            yonalish:"Frontend",
+//            quvvat:5998,
+//            qarzdorlik:"true"
+//         },
+//         {
+//             login:3425,
+//             parol:88727,
+//             ismi:"elbek",
+//             familya:"sodiqov",
+//             coin:2500,
+//             ustoz:"Sunnatbek",
+//             guruh:2094,
+//             yonalish:"Frontend",
+//             quvvat:5998,
+//             qarzdorlik:"true"
+//          },
+//          {
+//             login:34428,
+//             parol:86827,
+//             ismi:"nurmohammad",
+//             familya:"ubaydullayev",
+//             coin:2500,
+//             ustoz:"Sunnatbek",
+//             guruh:2094,
+//             yonalish:"Frontend",
+//             quvvat:5998,
+//             qarzdorlik:"true"
+//          },
+//          {
+//             login:30428,
+//             parol:85827,
+//             ismi:"kamron",
+//             familya:"burxonov",
+//             coin:2500,
+//             ustoz:"Sunnatbek",
+//             guruh:2094,
+//             yonalish:"Frontend",
+//             quvvat:5998,
+//             qarzdorlik:"true"
+//          },
+//          {
+//             login:30428,
+//             parol:8427,
+//             ismi:"alisher",
+//             familya:"ziyodillayev",
+//             coin:2500,
+//             ustoz:"Sunnatbek",
+//             guruh:2094,
+//             yonalish:"Frontend",
+//             quvvat:5998,
+//             qarzdorlik:"true"
+//          },
+//          {
+//             login:36428,
+//             parol:83827,
+//             ismi:"shukrullo",
+//             familya:"ubaydullayev",
+//             coin:2500,
+//             ustoz:"Sunnatbek",
+//             guruh:2094,
+//             yonalish:"Frontend",
+//             quvvat:5998,
+//             qarzdorlik:"true"
+//          },
+//          {
+//             login:36428,
+//             parol:82827,
+//             ismi:"abdulhafiz",
+//             familya:"abdunazarov",
+//             coin:2500,
+//             ustoz:"Sunnatbek",
+//             guruh:2094,
+//             yonalish:"Frontend",
+//             quvvat:5998,
+//             qarzdorlik:"true"
+//          },
+//          {
+//             login:34028,
+//             parol:81827,
+//             ismi:"sulaymon",
+//             familya:"Baxtiyorov",
+//             coin:2500,
+//             ustoz:"Sunnatbek",
+//             guruh:2094,
+//             yonalish:"Frontend",
+//             quvvat:5998,
+//             qarzdorlik:"true"
+//          },
+//          {
+//             login:36428,        
+//             parol:80827,
+//             ismi:"ozod",
+//             familya:"tillaboyev",
+//             coin:2500,
+//             ustoz:"Sunnatbek",
+//             guruh:2094,
+//             yonalish:"Frontend",
+//             quvvat:5998,
+//             qarzdorlik:"true"
+//          }
+//  ];
+
+ 
+
+//  oquvchilar.forEach(obyektlar=>{
+//     if(obyektlar.coin < 1500){
+//         console.log(obyektlar.ismi);
+
+//     }
+//  })
+
+
+
+// window.onload = function() {
+//    const messageContainer = document.getElementById('message-container');
+//    messageContainer.innerHTML = "Salom, bu oq ekranda chiqayotgan so'z!";
+// };
+
+// const username = prompt("ismingizni kriting");
+// const username2 = prompt("familya kriting");
+// const username3 = prompt("yosh kriting");
+// const isim = document.createElement("h1");
+// ism.textContent = "salom dom";
+// document.body.appendChild(ism);
+// alert("ismingizni kriting");
+
+// const familya = document.createElement("h1");
+// familya.textContent = "salom dom";
+// document.body.appendChild(familya);
+// alert("familyangizni kriting");
+
+// const yosh = document.createElement("h1");
+// yosh.textContent = "salom dom";
+// document.body.appendChild(yosh);
+// alert("yoshingizni kriting");
+
+
+const username = prompt ("ismingizni kiriting")
+const username1 = prompt ("familiyangizni kiriting")
+const username3 = prompt ("yosh  kiriting")      
+const h1 = document.createElement ("h1")
+const h2 = document.createElement ("h2")
+const h3 = document.createElement ("h3")
+h1 .textContent = username;
+document.body.append(h1)
+h2 .textContent = username1;
+document.body.append(h2)
+h3 .textContent = username3;
+document.body.append(h3)
+
+// const numbers = [32,67,98,102,43,27,34,71,31,76,13,100];
+
+// if(num % 2 == 0){
+//    alert(`%{num} soni juft`)
+// }else{
+//    alert(`%{num} soni toq`)
+// }
+// console.log(numbers);
+
+// / for ( let index = 0; index < cars.length; index++){
+   //     console.log(cars[index]);
+   // }
+   
+   
+   
+   // const son = 7;
+   
+   // if (son % 2 === 0) {
+   //     console.log(son + " juft son");
+   // } else {
+   //     console.log(son + " toq son");
+   // }
+   
+   // let start = 1;  
+   // let end = 10;   
+   
+   // for (let i = start; i <= end; i++) {
+   //     if (i % 2 === 0) {
+   //         console.log(i + " juft son");
+   //     } else {
+   //         console.log(i + " toq son");
+   //     }
+   // }
+   
+   
+   // console.log(boshlangichAray,"boshlangichAray");
+   // let juftaray = [32,67,98,102,43,27,34,71,31,76,13,100];
+   // let juftSonlar = boshlangichAray.filter(num => num % 2 === 0);
+   
+   // console.log("Juft sonlar:", juftSonlar);
+   
